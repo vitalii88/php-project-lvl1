a19f84efefa10d5bac7f08b91a53e43047ff53b2
Vitalii Bairak <vitalii_b88@yahoo.com>
2020-03-18 23:47:11 +0200

    add src dir. and update bin\brain-games

---
+++ src/Cli.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace BrainGames\Cli;
+
+function run()
+{
+    echo "Welcome to the Brain Games!", PHP_EOL;
+    echo "May I have your name? John";
+
+    $name = trim(fgets(STDIN));
+    echo "Hello, {$name}!", PHP_EOL;
+
+}
